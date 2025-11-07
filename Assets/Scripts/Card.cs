78d68f5610f820867c8d29510d00c0823f8fb832
Lord-Otter <ant99norgren@gmail.com>
2025-11-07 11:51:37 +0100

    Started work on card prefab

---
+++ Assets/Scripts/Card.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class Card : MonoBehaviour
+{
+    [Header("Card Sprites")]
+    [SerializeField]private Sprite[] faces;
+    [SerializeField] private Sprite cardBack;
+    
+    void Awake()
+    {
+        
+    }
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
