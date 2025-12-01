e13c5ff22d41f5e0c2389a9a09fae7b90d0d8332
Lord-Otter <ant99norgren@gmail.com>
2025-12-01 12:32:46 +0100

    same same

--- Assets/Scripts/Projectiles/ProjectileBehaviour.cs
+++ Assets/Scripts/Projectiles/ProjectileBehaviour.cs
@@ -6,6 +6,10 @@ namespace Spelprojekt1
     public class ProjectileBehaviour : MonoBehaviour
     {
         [SerializeField] private float destroyTimer;
+        [SerializeField] private float damage;
+        [SerializeField] private float Knockback;
+        [SerializeField] private float range;
+
         private Vector2 direction = new Vector2(0, 1);
 
         void Awake()
