94e6aefeefbbb47e737c0d942ca4b7b96b654910
Lord-Otter <ant99norgren@gmail.com>
2026-01-09 16:18:35 +0100

    EnemySpawn ändringar. Separerade mattor(props) till ett separat lager från andra props i Chamber3

--- Assets/Scripts/Managers/EnemyDeathNotifier.cs
+++ Assets/Scripts/Managers/EnemyDeathNotifier.cs
@@ -4,6 +4,11 @@ public class EnemyDeathNotifier : MonoBehaviour
 {
     public EnemySpawner spawner;
 
+    private void Start()
+    {
+        spawner = GameObject.Find("GameManager").GetComponent<EnemySpawner>();
+    }
+
     private void OnDestroy()
     {
         if (spawner != null)
